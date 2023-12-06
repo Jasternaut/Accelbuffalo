45d53bd91be4f254b0911226d5a89e9b470128fb
Руслан Патрушев <ruslik2806@outlook.com>
2023-12-06 11:10:58 +0200

    Крупное обновление. Добавлен основной функционал работы с документами, сохранение, редактирование и загрузка. Улучшен интерфейс.

--- MyDocs/MainWindow.xaml.cs
+++ MyDocs/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +14,21 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Xps.Packaging;
 
 namespace MyDocs
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        // передвижение окна по экрану мышкой
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -33,14 +37,88 @@ namespace MyDocs
             }
         }
 
+        // закрытие приложения
         private void Close_Button_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
 
+        // свернуть окно
         private void Minimize_Button_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void browse_Click(object sender, RoutedEventArgs e)
+        {
+            Document_Viewer.Text = "";
+
+            // открытие диалога для выбора документа
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Текстовые файлы (.txt)|*.txt";
+
+            // появление диалога и ожидание выбора
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                if (dlg.FileName.Length > 0)
+                {
+                    Document_Name.Content = dlg.FileName;
+                    CurrentDocument.Content = dlg.FileName;
+                    
+                    string[] line = File.ReadAllLines(dlg.FileName);
+
+                    foreach (string line2 in line)
+                    {
+                        Document_Viewer.Text += line2 + "\n";
+                    }                                                                        
+                }
+            }
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {       
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.Filter = "Текстовый файл (*.txt)|*.txt";
+
+            if (dlg.ShowDialog() == true)
+            {
+                string filename = dlg.FileName;
+                System.IO.File.WriteAllText(filename, Document_Viewer.Text);
+            }
+        }
+
+        private void Load_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Document_Viewer.Text = "";
+
+            // открытие диалога для выбора документа
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Текстовые файлы (.txt)|*.txt";
+
+            // появление диалога и ожидание выбора
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                if (dlg.FileName.Length > 0)
+                {
+                    Document_Name.Content = dlg.FileName;
+                    CurrentDocument.Content = dlg.FileName;
+
+                    string[] line = File.ReadAllLines(dlg.FileName);
+
+                    foreach (string line2 in line)
+                    {
+                        Document_Viewer.Text += line2 + "\n";
+                    }
+                }
+            }
         }
     }
 }
